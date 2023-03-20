af517fd067dac819cba04705661bd8da0d16d6c6
ravedappeport <dave.rappeport@vareto.com>
2023-03-19 19:34:13 -0700

    saving a checkpoint basically

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import ChessboardComponent from './components/Chessboard';
+import DailyLesson from './components/DailyLesson';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chess Lessons</h1>
       </header>
+      <main>
+        <DailyLesson />
+      </main>
     </div>
   );
 }
+
 
 export default App;
